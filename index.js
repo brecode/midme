4ce5ef073216ee15e2c73c33241f9dc8e3b2935a
Nikos <nikos.bre@gmail.com>
2014-06-24 15:50:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ var app = express();
 app.set('port', process.env.PORT || 8080); 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.bodyParser()); // <-- add
+app.use(express.bodyParser());
 
-var mongoHost = 'localHost'; //A
+var mongoHost = 'localHost'; 
 var mongoPort = 27017; 
 var collectionDriver;
  
-var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); //B
-mongoClient.open(function(err, mongoClient) { //C
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); 
+mongoClient.open(function(err, mongoClient) { 
   if (!mongoClient) {
       console.error("Error! Exiting... Must start MongoDB first");
-      process.exit(1); //D
+      process.exit(1); 
   }
-  var db = mongoClient.db("MyDatabase");  //E
-  collectionDriver = new CollectionDriver(db); //F
+  var db = mongoClient.db("MyDatabase");  
+  collectionDriver = new CollectionDriver(db); 
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -31,52 +31,52 @@ app.get('/', function (req, res) {
   res.send('<html><body><h1>Hello World</h1></body></html>');
 });
  
-app.get('/:collection', function(req, res) { //A
-   var params = req.params; //B
-   collectionDriver.findAll(req.params.collection, function(error, objs) { //C
-    	  if (error) { res.send(400, error); } //D
+app.get('/:collection', function(req, res) {
+   var params = req.params; 
+   collectionDriver.findAll(req.params.collection, function(error, objs) { 
+    	  if (error) { res.send(400, error); } 
 	      else { 
-	          if (req.accepts('html')) { //E
-    	          res.render('data',{objects: objs, collection: req.params.collection}); //F
+	          if (req.accepts('html')) { 
+    	          res.render('data',{objects: objs, collection: req.params.collection}); 
               } else {
-	          res.set('Content-Type','application/json'); //G
-                  res.send(200, objs); //H
+	          res.set('Content-Type','application/json'); 
+                  res.send(200, objs); 
               }
          }
    	});
 });
  
-app.get('/:collection/:entity', function(req, res) { //I
+app.get('/:collection/:entity', function(req, res) { 
    var params = req.params;
    var entity = params.entity;
    var collection = params.collection;
    if (entity) {
-       collectionDriver.get(collection, entity, function(error, objs) { //J
+       collectionDriver.get(collection, entity, function(error, objs) { 
           if (error) { res.send(400, error); }
-          else { res.send(200, objs); } //K
+          else { res.send(200, objs); } 
        });
    } else {
       res.send(400, {error: 'bad url', url: req.url});
    }
 });
 
-app.post('/:collection', function(req, res) { //A
+app.post('/:collection', function(req, res) { 
     var object = req.body;
     var collection = req.params.collection;
     collectionDriver.save(collection, object, function(err,docs) {
           if (err) { res.send(400, err); } 
-          else { res.send(201, docs); } //B
+          else { res.send(201, docs); } 
      });
 });
 
-app.put('/:collection/:entity', function(req, res) { //A
+app.put('/:collection/:entity', function(req, res) { 
     var params = req.params;
     var entity = params.entity;
     var collection = params.collection;
     if (entity) {
-       collectionDriver.update(collection, req.body, entity, function(error, objs) { //B
+       collectionDriver.update(collection, req.body, entity, function(error, objs) { 
           if (error) { res.send(400, error); }
-          else { res.send(200, objs); } //C
+          else { res.send(200, objs); } 
        });
    } else {
 	   var error = { "message" : "Cannot PUT a whole collection" }
@@ -84,14 +84,14 @@ app.put('/:collection/:entity', function(req, res) { //A
    }
 });
 
-app.delete('/:collection/:entity', function(req, res) { //A
+app.delete('/:collection/:entity', function(req, res) { 
     var params = req.params;
     var entity = params.entity;
     var collection = params.collection;
     if (entity) {
-       collectionDriver.delete(collection, entity, function(error, objs) { //B
+       collectionDriver.delete(collection, entity, function(error, objs) { 
           if (error) { res.send(400, error); }
-          else { res.send(200, objs); } //C 200 b/c includes the original doc
+          else { res.send(200, objs); } 
        });
    } else {
        var error = { "message" : "Cannot DELETE a whole collection" }
